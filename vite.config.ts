8316008b2241e72804e05bd0f249238b967ae318
catalintugui <catalindtugui@gmail.com>
2026-06-25 22:32:01 +0300

    fix gh error

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  base: '/initiativa-urbana/',
+export default defineConfig(({ command }) => ({
+  base: command === 'build' ? '/initiativa-urbana/' : '/',
   plugins: [react()],
-});
+}));
