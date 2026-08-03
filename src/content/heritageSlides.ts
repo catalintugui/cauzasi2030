9c47ea802ad6892c5ab65e8c8d0c6997af035ef6
catalintugui <catalindtugui@gmail.com>
2026-08-03 21:47:13 +0300

    replaced pictures

--- src/content/heritageSlides.ts
+++ src/content/heritageSlides.ts
@@ -2,27 +2,35 @@ import { withBase } from "../utils/withBase";
 
 export const heritageSlides = [
     {
-        src: withBase("/heritage/01-casa-istorica-langa-cladire-moderna.webp"),
-        alt: "Casă istorică galbenă lângă o clădire modernă albă",
+        src: withBase("/heritage/01-carmen-sylva-residence.webp"),
+        alt: "Ansamblu rezidențial modern Carmen Sylva cu curte interioară amenajată",
     },
     {
-        src: withBase("/heritage/02-vila-neoclasica-alba.webp"),
-        alt: "Vilă neoclasică albă cu gard și copaci",
+        src: withBase("/heritage/02-vila-neoclasica-crem.webp"),
+        alt: "Vilă neoclasică crem cu coloane, balcon și gard din fier forjat",
     },
     {
-        src: withBase("/heritage/03-liceul-matei-basarab.webp"),
-        alt: "Liceul Matei Basarab, clădire istorică acoperită de iarbă",
+        src: withBase("/heritage/03-fatada-galbena-degradata.webp"),
+        alt: "Fațadă galbenă degradată cu ferestre arcuite și stucaturi ornamentate",
     },
     {
-        src: withBase("/heritage/04-usa-art-deco-rosie.webp"),
-        alt: "Ușă Art Deco roșie cu detalii geometrice",
+        src: withBase("/heritage/04-fatada-ornamentata-crem.webp"),
+        alt: "Fațadă ornamente crem cu fereastră centrală arcuită și grilaje din fier",
     },
     {
-        src: withBase("/heritage/05-cladire-urbana-veche.webp"),
-        alt: "Clădire urbană veche cu balcon și ferestre înalte",
+        src: withBase("/heritage/05-intrare-istorica-marquise.webp"),
+        alt: "Intrare istorică cu marquise din fier forjat, lei sculptați și poartă",
     },
     {
-        src: withBase("/heritage/06-casa-neoromaneasca.webp"),
-        alt: "Casă neoromânească cu gard verde și bust comemorativ",
+        src: withBase("/heritage/06-usa-dubla-alba.webp"),
+        alt: "Ușă dublă albă cu arc sculptat și trepte de beton",
+    },
+    {
+        src: withBase("/heritage/07-poarta-fier-forjat.webp"),
+        alt: "Poartă din fier forjat la intrarea unei clădiri albe pe Str. Ion Filibiliu",
+    },
+    {
+        src: withBase("/heritage/08-cladire-colt-turla.webp"),
+        alt: "Clădire istorică de colț cu tură și balcoane din fier forjat",
     },
 ] as const;
